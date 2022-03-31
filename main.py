56d59fbfe3aed8baceca646edf14dc3f8a48ca60
els-richardwei <weir@science.regn.net>
2022-03-31 14:16:21 +0800

    fix: :ambulance: Credential leakage

--- main.py
+++ main.py
@@ -6,15 +6,15 @@ def test_get_favorite_article():
     
     client = boto3.client(
         'dynamodb',
-        aws_access_key_id='ASIAQI5MD4KIXVQBFXWW',
-        aws_secret_access_key='QmTqQRLRsmK4wbP0F1MMjppwpO53LE11mOJX3Eyn',
-        aws_session_token='IQoJb3JpZ2luX2VjEHgaDmNuLW5vcnRod2VzdC0xIkgwRgIhAOGC/lSZ+8He5b2XLWaBiUSSekPUKbK7u1Z/WaBDGYSiAiEAx1U4IzIlywbmEDi3RDqoTJpz7EVx8JG53LN22WZAoaAqzwIIpv//////////ARABGgwwMTkxNTE0NDY2NzMiDIQzjsygw5j1GGAkDyqjAilPwx+h6p3+tKqWzEoESH0XM+7oGPk+sfSp2Nn8NavAHOsxozI9Bkh0yCZPsCwrHRg2pWpuAUyn3yXw2mic7e34LU3hb32LyQjxiye3XLiHtLsbSS3Nofm4nzLqGK517pSf/fzddcYbtYVIDonV/gXsewiUDk8gbxOviyOJ0t2iVUrzpV/9ILVX+D4G8kQQQDIZX3bbFv8SAqI1yhSAcWypzvD9xOGT+95yvWnuMzm0HuvNhFaL17FPeyAl89cxoAKSavaPaEq9Ui97ehvXyBiIkXfTaAZHVBpEEfDSMFSaj7H13M/V+dcHf6f14CiR4bz4NwO9QjPwNEWkInBfyxAV7NyPnIzZcqw7oDVvpk3/FE4rS2jbWoWII6ZC9pb69QeIUzDh8ZSSBjqhAdQf6OGAR0ncmdu1EIBrjOMFQe4s3+J5TuFU42eL18Yg3zPAMhKS5T6rOEI3FtPl3CaAwm9L/gSnYBw8yK5zthVMCBEVj7QXlrHlYCTwp2vcvDV97XcKAHWs4RE9Gycvdz29vPVv0qgkUG6lvvvLHKBibm5Wnmw0qujDLbvOQbWU9gVCBjgzqiZcm1xQ+RqAz7Z/bNjOJTrhsZ+6Mb4jHS2h',
+        aws_access_key_id='*********',
+        aws_secret_access_key='****',
+        aws_session_token='********',
     )
     dynamodb = boto3.resource(
         'dynamodb',
-        aws_access_key_id='ASIAQI5MD4KIXVQBFXWW',
-        aws_secret_access_key='QmTqQRLRsmK4wbP0F1MMjppwpO53LE11mOJX3Eyn',
-        aws_session_token='IQoJb3JpZ2luX2VjEHgaDmNuLW5vcnRod2VzdC0xIkgwRgIhAOGC/lSZ+8He5b2XLWaBiUSSekPUKbK7u1Z/WaBDGYSiAiEAx1U4IzIlywbmEDi3RDqoTJpz7EVx8JG53LN22WZAoaAqzwIIpv//////////ARABGgwwMTkxNTE0NDY2NzMiDIQzjsygw5j1GGAkDyqjAilPwx+h6p3+tKqWzEoESH0XM+7oGPk+sfSp2Nn8NavAHOsxozI9Bkh0yCZPsCwrHRg2pWpuAUyn3yXw2mic7e34LU3hb32LyQjxiye3XLiHtLsbSS3Nofm4nzLqGK517pSf/fzddcYbtYVIDonV/gXsewiUDk8gbxOviyOJ0t2iVUrzpV/9ILVX+D4G8kQQQDIZX3bbFv8SAqI1yhSAcWypzvD9xOGT+95yvWnuMzm0HuvNhFaL17FPeyAl89cxoAKSavaPaEq9Ui97ehvXyBiIkXfTaAZHVBpEEfDSMFSaj7H13M/V+dcHf6f14CiR4bz4NwO9QjPwNEWkInBfyxAV7NyPnIzZcqw7oDVvpk3/FE4rS2jbWoWII6ZC9pb69QeIUzDh8ZSSBjqhAdQf6OGAR0ncmdu1EIBrjOMFQe4s3+J5TuFU42eL18Yg3zPAMhKS5T6rOEI3FtPl3CaAwm9L/gSnYBw8yK5zthVMCBEVj7QXlrHlYCTwp2vcvDV97XcKAHWs4RE9Gycvdz29vPVv0qgkUG6lvvvLHKBibm5Wnmw0qujDLbvOQbWU9gVCBjgzqiZcm1xQ+RqAz7Z/bNjOJTrhsZ+6Mb4jHS2h',
+        aws_access_key_id='*****',
+        aws_secret_access_key='*****',
+        aws_session_token='******',
     )
     
     ddb_exceptions = client.exceptions
